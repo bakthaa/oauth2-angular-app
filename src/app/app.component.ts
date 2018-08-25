f27662b60697f25a4de6670eeff5a69ca5722bcf
power-team <power-team@samba-localdomain.hsd1.mi.comcast.net>
2018-08-25 00:11:30 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'oaas-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'oauth2-angular-app';
+}
